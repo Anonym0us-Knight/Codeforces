8f23d9852aefe34c29e5195def3ae2081ff32a64
Pushp Raj <69637880+Anonym0us-Knight@users.noreply.github.com>
2021-04-30 10:37:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Codeforces
+ This repository contains the codes that I wrote while solving codeforces problems.
---
+++ Untitled2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int c;
+    cin>>a;
+    for(int i=0;i<a;i++){
+        cin>>b>>c;
+
+
+        cout<<b<<" "<<(b*2)<<endl;
+    }
+
+    return 0;
+}
